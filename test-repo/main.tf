d95027a79c3422a16d0824821c59e3e61d5c8fd4
Rameez <rameezts@gmail.com>
2022-03-31 08:38:25 +0800

    update test repo

---
+++ test-repo/main.tf
@@ -0,0 +1,46 @@
+terraform {
+required_providers {
+  
+  aws = {
+      source = "hashicorp/aws"
+  }
+}
+  cloud {
+    organization = "poc-rameez"
+
+    workspaces {
+      name = "testpoc"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region 
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls_terraform-poc"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0e2be66b"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol           = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls_terraform-poc"
+  }
+}
